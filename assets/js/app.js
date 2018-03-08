dec0d22702cab1744a012080f626a4040e27ea95
alextwilson <github@alextwilson.co.uk>
2018-03-08 14:27:20 +0000

    Removes imports from app where not used

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 import "phoenix_html"
-import { connect } from 'react-redux';
-import React, {PropTypes} from "react"
+import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import axios from "axios"
