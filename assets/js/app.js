ce9d8b1a42756c1c3c812fd50812ceb4e30582b2
alextwilson <github@alextwilson.co.uk>
2018-03-08 13:42:00 +0000

    Has working react web form
    Does not post to api
    submit updates form state
    
    Co-authored-by: GiadaSimonetti <giada.simonetti@gmail.com>
    Co-authored-by: AlexandraGF <alexandra.fina@yahoo.com>
    Co-authored-by: lewmoore <lewis.moore2707@gmail.com>
    Co-authored-by: bpourian <b.pourian@hotmail.co.uk>
    Co-authored-by: MissDove <agata.golebiewska@yahoo.co.uk>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,92 @@
 import "phoenix_html"
-
-import React from "react"
+import { connect } from 'react-redux';
+import React, {PropTypes} from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import axios from "axios"
+
 class HelloReact extends React.Component {
   render() {
-    return (<h1>Hello React!</h1>)
+    return (
+      <Router>
+      <div>
+        <Route exact path="/" component={Home}/>
+        <Route path="/books/new" component={NewBook}/>
+      </div>
+      </Router>
+    )
   }
 }
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to PickABook!</h1>
+
+        <Link to="/books/new">Add a book</Link>
+      </div>
+    )
+  }
+}
+
+class NewBook extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      author: '',
+      genre: '',
+      description: ''
+    };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.state = {
+      title: this.refs.title.value,
+      author: this.refs.author.value,
+      genre: this.refs.genre.value,
+      description: this.refs.description.value
+    };
+    console.log(this.state);
+    // axios.post('http://localhost:4000/api/books', {
+    // headers: {"Content-Type": "application/json"},
+    // data: {
+    //   books: {
+    //     title: this.state.title,
+    //     author: this.state.author,
+    //     genre: this.state.genre,
+    //     description: this.state.description
+    //   }
+    // }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add a book</h1>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div className="field">
+            <input ref="title" type="text" placeholder="Title" required={true} />
+          </div>
+          <div className="field">
+            <input ref="author" type="text" placeholder="Author" required={true} />
+          </div>
+          <div className="field">
+            <input ref="genre" type="text" placeholder="Genre" required={true} />
+          </div>
+          <div className="field">
+            <input ref="description" type="text" placeholder="Description" required={true} />
+          </div>
+          <button type="submit">Add book</button>
+        </form>
+        <Link to="/">Home</Link>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
   <HelloReact/>,
   document.getElementById("hello-react")
