4869e28c18526ed52d60606cd2eeb9dbad71f3e6
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-10 12:56:06 +0000

    Add enzyme to the tests

--- assets/tests/app.test.js
+++ assets/tests/app.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Enzyme from 'enzyme';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import HelloReact from '../js/app';
 
-describe('Home', () => {
+describe('HelloReact', () => {
   it('renders without crashing', () => {
     const div = document.createElement("div");
     div.id = "main";
     const window = ReactDom.render(<HelloReact/>, div);
+  });
+
+  it('404 invalid route', () => {
+
   });
 });
