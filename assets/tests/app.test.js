034d6e3a64d4b79a5229ff6a9fe2789e15453031
Alexy <alexandra.fina@yahoo.com>
2018-03-10 13:07:30 +0000

    Test for invalid route 404

--- assets/tests/app.test.js
+++ assets/tests/app.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Enzyme from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import HelloReact from '../js/app';
 
@@ -12,6 +13,12 @@ describe('HelloReact', () => {
   });
 
   it('404 invalid route', () => {
-
+    const wrapper = mount(
+    <Router initialEntries={[ '/random' ]}>
+      <HelloReact/>
+    </Router>
+  );
+  expect(wrapper.find(LandingPage)).toHaveLength(0);
+  expect(wrapper.find(NotFoundPage)).toHaveLength(1);
   });
 });
