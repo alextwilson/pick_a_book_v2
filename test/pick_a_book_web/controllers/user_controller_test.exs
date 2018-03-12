035fc4d07617beb6350ac220afe627daf43e60b4
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-12 12:31:24 +0000

    Remove redundant code and refactor

--- test/pick_a_book_web/controllers/user_controller_test.exs
+++ test/pick_a_book_web/controllers/user_controller_test.exs
@@ -2,31 +2,13 @@ defmodule PickABookWeb.UserControllerTest do
   use PickABookWeb.ConnCase
 
   alias PickABook.Accounts
-  alias PickABook.Accounts.User
-  alias PickABook.Accounts.APICall
 
-  @create_attrs %{email: "email@email.com", username: "username", password: "somepassword", password_confirmation: "somepassword"}
-  @update_attrs %{email: "some updated email", password_hash: "some updated password_hash", username: "some updated username"}
   @invalid_attrs %{email: nil, username: nil, password: nil, password_confirmation: nil}
-
-  def fixture(:user) do
-    {:ok, user} = Accounts.create_user(@create_attrs)
-    user
-  end
-
-  setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
-  end
 
   describe "create user" do
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post conn, user_path(conn, :create), user: @invalid_attrs
       assert json_response(conn, 422)["errors"] != %{}
     end
-  end
-
-  defp create_user(_) do
-    user = fixture(:user)
-    {:ok, user: user}
   end
 end
