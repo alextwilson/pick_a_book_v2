ea18bb74d54deae31f85ca64eab7be2b84202f82
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-13 10:31:15 +0000

    Fixes bug created by routing and create a book

--- lib/pick_a_book_web/router.ex
+++ lib/pick_a_book_web/router.ex
@@ -30,9 +30,9 @@ defmodule PickABookWeb.Router do
 
     post "/sign_up", UserController, :create
     post "/sign_in", UserController, :sign_in
-
-    resources "/books", BookController, except: [:new, :edit, :create]
     post "/book/new", BookController, :create
+    resources "/books", BookController, except: [:new, :edit]
+
   end
 
   scope "/api", PickABookWeb do
