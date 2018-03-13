fe915c159b965fc8fcd42f77db9ce25a7655e962
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-13 14:22:29 +0000

    Add url field to new book form

--- assets/js/components/newBook.js
+++ assets/js/components/newBook.js
@@ -1,26 +1,25 @@
-import 'phoenix_html'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import axios from "axios"
+import "phoenix_html";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from "axios";
 
 class NewBook extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     axios({
-      method: 'post',
-      headers: {"Content-Type": "application/json"},
-      url: '/api/books',
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      url: "/api/books",
       data: {
         book: {
           title: this.refs.title.value,
           author: this.refs.author.value,
           genre: this.refs.genre.value,
-          description: this.refs.description.value,
-          imageurl: this.refs.imageurl.value
+          description: this.refs.description.value
         }
       }
-    })
+    });
   }
 
   render() {
@@ -29,23 +28,49 @@ class NewBook extends React.Component {
         <h1>Add a book</h1>
         <form onSubmit={this.handleSubmit.bind(this)}>
           <div className="field">
-            <input ref="title" type="text" placeholder="Title" required={true} />
+            <input
+              ref="title"
+              type="text"
+              placeholder="Title"
+              required={true}
+            />
           </div>
           <div className="field">
-            <input ref="author" type="text" placeholder="Author" required={true} />
+            <input
+              ref="author"
+              type="text"
+              placeholder="Author"
+              required={true}
+            />
           </div>
           <div className="field">
-            <input ref="genre" type="text" placeholder="Genre" required={true} />
+            <input
+              ref="genre"
+              type="text"
+              placeholder="Genre"
+              required={true}
+            />
           </div>
           <div className="field">
-            <input ref="description" type="text" placeholder="Description" required={true} />
+            <input
+              ref="description"
+              type="text"
+              placeholder="Description"
+              required={true}
+            />
           </div>
           <div className="field">
-            <input ref="imageurl" type="text" placeholder="image url" required={true} />
-          </div>
-          <button type="submit">Add book</button>
+            <input
+              ref="imageurl"
+              type="text"
+              placeholder="Image URL"
+              required={true}
+              />
+            </div>
+            <button type="submit">Add book  </button>
+
         </form>
-        <Link to="/">Home</Link>
+
       </div>
     );
   }
