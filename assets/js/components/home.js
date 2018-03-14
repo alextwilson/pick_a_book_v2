f1578a502829abb8cbf61a991dab5fc19bbd07e3
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-03-14 18:14:13 +0000

    Change blockquote

--- assets/js/components/home.js
+++ assets/js/components/home.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import styled from "styled-components";
 import Title from "./styled/title";
-// import Background from "./styled/background";
 
 class Home extends React.Component {
   render() {
@@ -14,14 +13,34 @@ class Home extends React.Component {
         {sessionStorage.getItem("username") && (
           <Title primary>Hello {sessionStorage.getItem("username")}!</Title>
         )}
-        <h4>Rather than leave your books on the shelf never to be opened again, or worse, to be thrown away, try swapping them using PickABook!</h4><br/>
-        <p>Join today and swap books with other members of our community. PickABook helps you by giving away books you no longer need in exchange for books you really want.</p><br/>
+        <h4>
+          Rather than leave your books on the shelf never to be opened again, or
+          worse, to be thrown away, try swapping them using PickABook!
+        </h4>
+        <br />
+        <p>
+          Join today and swap books with other members of our community.
+          PickABook helps you by giving away books you no longer need in
+          exchange for books you really want.
+        </p>
+        <br />
         <h4>Interesting facts about books:</h4>
         <blockquote>There are over 129 million books in existence.</blockquote>
-        <blockquote>The first book ever written using a typewriter was The Adventures of Tom Sawyer.</blockquote>
-        <blockquote>Books used to be chained to shelves in libraries, in order to prevent stealing.</blockquote>
-        <blockquote>An estimated 755,755 new books are published every year.</blockquote>
-        <blockquote>The Japanese word ‘tsundoku’ apparently means “buying lots of books and then never getting around to reading them.</blockquote>
+        <blockquote>
+          The first book ever written using a typewriter was The Adventures of
+          Tom Sawyer.
+        </blockquote>
+        <blockquote>
+          Books used to be chained to shelves in libraries, in order to prevent
+          stealing.
+        </blockquote>
+        <blockquote>
+          An estimated 755,755 new books are published every year.
+        </blockquote>
+        <blockquote>
+          The Japanese word ‘tsundoku’ apparently means “buying lots of books
+          and then never getting around to reading them.
+        </blockquote>
       </div>
     );
   }
