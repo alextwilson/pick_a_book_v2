21190fd71b63130514b885ac8d2a617c24f535a6
Alexy <alexandra.fina@yahoo.com>
2018-03-14 16:46:32 +0000

    Add facts about books

--- assets/js/components/home.js
+++ assets/js/components/home.js
@@ -7,12 +7,18 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <h3>Welcome to PickABook!</h3>
+        <h3>Welcome to PickABook!</h3><br/>
         {sessionStorage.getItem("username") && (
           <h4>Hello {sessionStorage.getItem("username")}!</h4>
         )}
-        <h4>Rather than leave your books on the shelf never to be opened again, or worse, throw them away, try swapping them using PickABook!</h4><br/>
-        <p>Join today and swap books with other members of our community. PickABook helps you giving away books you no longer need in exchange for books you really want.</p>
+        <h4>Rather than leave your books on the shelf never to be opened again, or worse, to be thrown away, try swapping them using PickABook!</h4><br/>
+        <p>Join today and swap books with other members of our community. PickABook helps you by giving away books you no longer need in exchange for books you really want.</p><br/>
+        <h4>Interesting facts about books:</h4>
+        <blockquote>There are over 129 million books in existence.</blockquote>
+        <blockquote>The first book ever written using a typewriter was The Adventures of Tom Sawyer.</blockquote>
+        <blockquote>Books used to be chained to shelves in libraries, in order to prevent stealing.</blockquote>
+        <blockquote>An estimated 755,755 new books are published every year</blockquote>
+        <blockquote>The Japanese word ‘tsundoku’ apparently means “buying lots of books and then never getting around to reading them.</blockquote>
       </div>
     );
   }
